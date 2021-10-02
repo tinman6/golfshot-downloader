3968aa8dd9a8913013eb3cd29f5a8342749c55a0
tinman6 <tinman6@users.noreply.github.com>
2021-10-02 07:08:15 -0400

    added parser for golfshot course

--- golfshot-downloader.py
+++ golfshot-downloader.py
@@ -8,13 +8,31 @@ import pprint
 
 class ScorecardParser(HTMLParser):
   def handle_data(self, data):
-    if data.startswith('ReactDOM.hydrate(React.createElement(Golfshot.Applications.Scorecard,'):
-      # the golfshot model is available in a script block
-      model = json.loads(data.partition('\n')[0][70:-57])  # counting chars is not great, but simpliest
-      self.results = model
+    # the golfshot model is available in a script block
+    if 'Golfshot.Applications.Scorecard' in data:
+      for line in data.splitlines():
+        if 'Golfshot.Applications.Scorecard' in line:
+          # counting chars is not great, but simpliest
+          model = json.loads(line[70:-57])
+          self.results = model
+
+
+class CourseParser(HTMLParser):
+  def handle_data(self, data):
+    if 'Golfshot.Applications.CourseScorecard' in data:
+      for line in data.splitlines():
+        if 'Golfshot.Applications.CourseScorecard' in line:
+          model = json.loads(line[76:-59])
+          self.results = model
 
 
 with open('mocks/scorecard.html') as mock_scorecard:
   p = ScorecardParser()
   p.feed(mock_scorecard.read())
+  pprint.pprint(p.results)
+
+
+with open('mocks/course.html') as mock_course:
+  p = CourseParser()
+  p.feed(mock_course.read())
   pprint.pprint(p.results)

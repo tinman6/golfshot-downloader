29f9dee731b56e5eabfe55f7710337f113af8f89
tinman6 <tinman6@users.noreply.github.com>
2021-10-03 18:14:32 -0400

    added iterator to download all rounds

--- golfshot-downloader.py
+++ golfshot-downloader.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
 import argparse
+from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import json
 import re
 import requests
+
+GOLFSHOT_URL = 'https://play.golfshot.com'
 
 
 class RoundParser(HTMLParser):
@@ -22,10 +25,10 @@ class CourseParser(HTMLParser):
       self.results = json.loads(model)
 
 
-def download_course(course_id, session):
-  COURSE_URL = 'https://play.golfshot.com/courses/%s'
+def download_course(session, course_id):
+  COURSE_URL = f'{GOLFSHOT_URL}/courses/{course_id}'
 
-  res = session.get(COURSE_URL % course_id)
+  res = session.get(COURSE_URL)
   p = CourseParser()
   p.feed(res.text)
 
@@ -39,14 +42,40 @@ def download_course(course_id, session):
     json.dump(ret, f)
 
 
-def download_round(url, session):
-  res = session.get(url)
+def download_round(session, profile_id, round_id):
+  ROUND_URL = f'{GOLFSHOT_URL}/profiles/{profile_id}/rounds/{round_id}'
+
+  res = session.get(ROUND_URL)
   p = RoundParser()
   p.feed(res.text)
   with open('data/rounds/%s.json' % p.results['roundGroupId'], 'w') as f:
     json.dump(p.results, f)
 
-  download_course(p.results['model']['detail']['golfCourseWebId'], session)
+  download_course(session, p.results['model']['detail']['golfCourseWebId'])
+
+
+def download_rounds(session, profile_id, last_round=None):
+  ROUNDS_URL = f'{GOLFSHOT_URL}/profiles/{profile_id}/rounds'
+  params = {'sb': 'Date', 'sd': 'Descending', 'p': 1}
+
+  download_rounds = True
+  while download_rounds:
+    rounds_html = session.get(ROUNDS_URL, data=params).text
+    soup = BeautifulSoup(rounds_html, 'html.parser')
+    round_table = soup.find('table', {'class': 'search-results'})
+
+    if not round_table:
+      download_rounds = False
+      break
+
+    for row in round_table.tbody.findAll('tr'):
+      round_id = row.attrs['data-href'].split('/')[-1]
+      if round_id == last_round:
+        download_rounds = False
+        break
+      download_round(session, profile_id, round_id)
+
+    params['p'] += 1
 
 
 
@@ -55,14 +84,12 @@ def download_round(url, session):
 parser = argparse.ArgumentParser(description='Download GolfShot data')
 parser.add_argument('username', help='Username for GolfShot account')
 parser.add_argument('password', help='Password for GolfShot account')
+parser.add_argument('profile_id', help='Profile ID for GolfShot account')
+parser.add_argument('--until', help='Download rounds until specified round (by descending date)')
 args = parser.parse_args()
 
-
-GOLFSHOT_URL = 'https://play.golfshot.com'
-
 with requests.Session() as session:
-  signin = session.post('%s/signin' % GOLFSHOT_URL,
+  signin = session.post(f'{GOLFSHOT_URL}/signin',
                         data={'Email': args.username,
                               'Password': args.password})
-
-  download_round('ROUND_URL', session)
+  download_rounds(session, args.profile_id, args.until)
